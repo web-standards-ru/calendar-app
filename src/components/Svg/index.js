857e3a528cf62ef49ec17fe50df95284ec1f7bf9
Dmitriy Shved <dshved@hotmail.com>
2018-01-28 10:30:14 +0300

    Resolve paths

--- src/components/Svg/index.js
+++ src/components/Svg/index.js
@@ -1,14 +1,13 @@
 import React from 'react'
 
-const Svg = ({ children, width, height, className }) => (
+const Svg = ({children, width, height, className}) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={width}
     height={height}
     viewBox={`0 0 ${width} ${height}`}
     className={className}
-    role="presentation"
-  >
+    role="presentation">
     {children}
   </svg>
 )
